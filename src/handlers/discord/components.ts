7c0ec9bbb2e5ebf5a8dc671cacb97bb9e07df82a
lisekilis <101191226+lisekilis@users.noreply.github.com>
2025-03-25 09:13:34 +0100

    wrong id?

--- src/handlers/discord/components.ts
+++ src/handlers/discord/components.ts
@@ -55,7 +55,7 @@ export async function handleMessageComponent(interaction: APIMessageComponentInt
 			if (!embed.image || !embed.image.url)
 				return messageResponse('The submission lacks attachments, how bizarre!', MessageFlags.Ephemeral);
 			const messageReResponse = await fetch(
-				`https://discord.com/api/v10/webhooks/${interaction.application_id}/${interaction.token}/messages/@original`
+				`https://discord.com/api/v10/webhooks/${env.DISCORD_APP_ID}/${interaction.token}/messages/@original`
 			);
 			console.log(await messageReResponse.text());
 			const message = (await (messageReResponse.ok ? messageReResponse.json() : messageReResponse.json())) as APIMessage;
@@ -108,24 +108,21 @@ export async function handleMessageComponent(interaction: APIMessageComponentInt
 				timestamp: new Date().toISOString(),
 			};
 
-			const response = await fetch(
-				`https://discord.com/api/v10/webhooks/${interaction.application_id}/${interaction.token}/messages/@original`,
-				{
-					method: 'PATCH',
-					headers: {
-						'Content-Type': 'application/json',
-					},
-					body: JSON.stringify({
-						embeds: [newEmbed],
-						components: [],
-						attachments: [
-							{
-								id: interaction.message.attachments[0].id,
-							},
-						],
-					}),
-				}
-			);
+			const response = await fetch(`https://discord.com/api/v10/webhooks/${env.DISCORD_APP_ID}/${interaction.token}/messages/@original`, {
+				method: 'PATCH',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					embeds: [newEmbed],
+					components: [],
+					attachments: [
+						{
+							id: interaction.message.attachments[0].id,
+						},
+					],
+				}),
+			});
 			console.log(await response.text());
 			if (!response.ok) {
 				console.error(`Error updating message: ${await response.text()}`);
