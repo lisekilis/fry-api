2295bf9631328445d7e4682f29bb19010a6b29e3
lisekilis <101191226+lisekilis@users.noreply.github.com>
2025-06-04 15:11:34 +0200

    눈_눈

--- src/discord/index.ts
+++ src/discord/index.ts
@@ -3,10 +3,11 @@ import commands from './commands';
 import { APIInteraction } from 'discord-api-types/v10';
 
 export default async function (request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+	const publicKey = env.DISCORD_PUBLIC_KEY.get();
 	const signature = request.headers.get('X-Signature-Ed25519');
 	const timestamp = request.headers.get('X-Signature-Timestamp');
 	const body = await request.text();
-	if (!verifyKey(body, signature || '', timestamp || '', await env.DISCORD_PUBLIC_KEY.get()))
+	if (!signature || !timestamp || !verifyKey(body, signature, timestamp, await publicKey))
 		return new Response('Unauthorized', { status: 401 });
 
 	const interaction = JSON.parse(body) as APIInteraction;
