eb6c71a889ecb9f41b95fa90c14ebe1b3acef326
lisekilis <101191226+lisekilis@users.noreply.github.com>
2025-06-04 15:34:34 +0200

    ??

--- src/discord/commands/index.ts
+++ src/discord/commands/index.ts
@@ -216,10 +216,10 @@ async function importCommandModule(commandName: string): Promise<any> {
 		// this allows for both TypeScript and JavaScript command files
 		// and allows for easier development without needing to compile TypeScript
 
-		return await import(`./${commandName}.ts`);
+		return (await import(`./${commandName}.ts`)).default;
 	} catch (tsError) {
 		try {
-			return await import(`./${commandName}.js`);
+			return (await import(`./${commandName}.js`)).default;
 		} catch (jsError) {
 			console.error(`Failed to import command module ${commandName}.ts:`, tsError);
 			console.error(`Failed to import command module ${commandName}.js:`, jsError);
